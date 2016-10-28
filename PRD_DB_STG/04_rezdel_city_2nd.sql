58ccb5e74a5ca9d96852fa418f19afef5a419095
Developer <klevtsovstanislav@gmial.com>
2016-10-28 23:24:22 +0300

    Create 04_rezdel_city_2nd.sql

---
+++ PRD_DB_STG/04_rezdel_city_2nd.sql
@@ -0,0 +1,15 @@
+-- Table: "PRD_DB_STG"."04_rezdel_city_2nd"
+
+-- DROP TABLE "PRD_DB_STG"."04_rezdel_city_2nd";
+
+CREATE TABLE "PRD_DB_STG"."04_rezdel_city_2nd"
+(
+  city character varying[],
+  id integer[],
+  cnt integer[]
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE "PRD_DB_STG"."04_rezdel_city_2nd"
+  OWNER TO postgres;
