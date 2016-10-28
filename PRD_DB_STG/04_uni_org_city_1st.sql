aebc57b9c9949b94535741494ad97b61ea106a39
Developer <klevtsovstanislav@gmial.com>
2016-10-28 23:26:01 +0300

    Create 04_uni_org_city_1st.sql

---
+++ PRD_DB_STG/04_uni_org_city_1st.sql
@@ -0,0 +1,14 @@
+-- Table: "PRD_DB_STG"."04_uni_org_city_1st"
+
+-- DROP TABLE "PRD_DB_STG"."04_uni_org_city_1st";
+
+CREATE TABLE "PRD_DB_STG"."04_uni_org_city_1st"
+(
+  id_sq bigint[],
+  sq_cnt integer[]
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE "PRD_DB_STG"."04_uni_org_city_1st"
+  OWNER TO postgres;
