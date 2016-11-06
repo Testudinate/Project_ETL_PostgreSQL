8718bb7c10705274579102523da595ad57b74023
Developer <klevtsovstanislav@gmial.com>
2016-11-06 16:17:47 +0300

    Add files via upload

--- 01_create_path_06_flats.py
+++ 01_create_path_06_flats.py
@@ -14,6 +14,7 @@ for top, dir, files in os.walk('C:\Load\PROCESSING'):
                 for fl1 in files:
                     print(fl1,top)
                     cursor.execute('''INSERT INTO "PRD_DB_DQ"."01_dq_create_path" \
-                    VALUES (now(),'NONE','01_insert_06_flats.py',' '''+ top +"','"+fl1+"')")
+                    (src_change_dtm, status, prc_name, upd_dtm,prc_id, full_path, file_name)\
+                    VALUES (now(),'NONE','01_insert_06_flats.py',now(),15,' '''+ top +"','"+fl1+"')")
 connect.commit()
 connect.close()
