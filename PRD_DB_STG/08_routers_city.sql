2dd49bdea1c78384ed2468dd3800d2ee92165df9
Developer <klevtsovstanislav@gmial.com>
2016-11-06 16:14:34 +0300

    Update 08_routers_city.sql

--- PRD_DB_STG/08_routers_city.sql
+++ PRD_DB_STG/08_routers_city.sql
@@ -1,33 +1,34 @@
--- Table: "PRD_DB_STG"."08_routers_city"
+ -- Table: "PRD_DB_STG"."08_routers_city"
 
 -- DROP TABLE "PRD_DB_STG"."08_routers_city";
 
 CREATE TABLE "PRD_DB_STG"."08_routers_city"
 (
-  id_sq bigint[],
-  city character varying[],
-  "0-100" integer[],
-  "100-200" integer[],
-  "200-300" integer[],
-  "300-400" integer[],
-  "400-500" integer[],
-  "500-600" integer[],
-  "700-800" integer[],
-  "800-900" integer[],
-  "900-1000" integer[],
-  "1000-1100" integer[],
-  "1100-1200" integer[],
-  "1200-1300" integer[],
-  "1300-1400" integer[],
-  "1400-1500" integer[],
-  "1500-1600" integer[],
-  "1600-1700" integer[],
-  "1700-1800" integer[],
-  "1800-1900" integer[],
-  "1900-2000" integer[]
+  id_sq bigint,
+  city text,
+  "0-100" integer,
+  "100-200" integer,
+  "200-300" integer,
+  "300-400" integer,
+  "400-500" integer,
+  "500-600" integer,
+  "700-800" integer,
+  "800-900" integer,
+  "900-1000" integer,
+  "1000-1100" integer,
+  "1100-1200" integer,
+  "1200-1300" integer,
+  "1300-1400" integer,
+  "1400-1500" integer,
+  "1500-1600" integer,
+  "1600-1700" integer,
+  "1700-1800" integer,
+  "1800-1900" integer,
+  "1900-2000" integer
 )
 WITH (
   OIDS=FALSE
 );
 ALTER TABLE "PRD_DB_STG"."08_routers_city"
   OWNER TO postgres;
+
