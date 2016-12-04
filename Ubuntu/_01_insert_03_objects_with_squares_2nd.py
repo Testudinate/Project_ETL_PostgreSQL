62b24a60a744dfdfc32b80c30c7cebcd5cf115ce
Developer <klevtsovstanislav@gmial.com>
2016-12-04 21:24:34 +0300

    Add files via upload

--- Ubuntu/_01_insert_03_objects_with_squares_2nd.py
+++ Ubuntu/_01_insert_03_objects_with_squares_2nd.py
@@ -10,13 +10,13 @@ connect = psycopg2.connect(database='pj', user='admin_etl', host='localhost', pa
 cursor = connect.cursor()
 
 cursor.execute('''SELECT count(1) FROM "prd_db_dq"."01_dq_create_path"
-               where prc_id = 11 and status='NONE' ''')
+               where prc_id = 4 and status='NONE' limit 1''')
 
 k=0
 for row in cursor.fetchall():
     if row[0] > 0:
         cursor.execute('''SELECT full_path, file_name,row_id FROM "prd_db_dq"."01_dq_create_path"
-               where prc_id = 11 and status='NONE' ''')
+               where prc_id = 4 and status='NONE' limit 1 ''')
         for rowi in cursor.fetchall():
             load_file = str(rowi[0]+"/"+rowi[1])
             load_file = load_file.replace(' /','/')
@@ -27,18 +27,24 @@ for row in cursor.fetchall():
             connect.commit()
             with codecs.open(load_file, 'r',encoding='utf-8',errors='ignore') as f:
                 csv_data = csv.reader(f,delimiter='|') # читаем данные из файла и задаем разделитель ';'
+                #next(csv_data) # игнорируем 1 строку, содержащую описание столбцов в файле
                 k=k+1
                 for row2 in csv_data:
-                    #print(row2[1])
+                    #print(row2[0])
                     #print((row2))
-                    #str(row2[0])
-                    s = "VALUES("+str(row2[0])+","+str(row2[1])+","+str(row2[2])+")"
-                    s0 =  'INSERT INTO "prd_db_stg"."04_uni_org_city_1st" (city , id_sq, sq_cnt) '
+                    s = "VALUES("+ str(row2[0])+",'"+str(row2[1])+"','"+str(row2[2])+"','"+str(row2[3])+"','"\
+                                 +str(row2[4])+"','"+str(row2[5])+"','"+str(row2[6])+"','"+str(row2[7])+"','"\
+                                 +str(row2[8])+"','"+str(row2[9])+"','"+str(row2[10])+"','"+str(row2[11])+"','"\
+                                 +str(row2[12])+"','"+str(row2[13])+"','"+str(row2[14])+"','"+str(row2[15])+"','"\
+                                 +str(row2[16])+"','"+str(row2[17])+"','"+str(row2[18])+"','"+str(row2[19])+"','"\
+                                 +str(row2[20])+"','"+str(row2[21])+"',"+str(row2[22])+",'"+str(row2[23])+"','"+str(row2[24])+"','"+str(row2[25])+"','"+str(row2[26])+"')"
+                    s0 =  'INSERT INTO "prd_db_stg"."03_objects_with_squares_2nd" (city, num ,name ,type_name, country ,region ,community ,district, postcode ,'\
+                           'address ,division ,rubric ,phones ,faxs,email ,'\
+                                 'web ,vk ,twitter ,facebook ,instagram ,"X" , "Y",id_sq,y1,y2,y3,y4)'
                     sql_code_1 = s0 + s         
                     #print(sql_code_1)
                     cursor.execute(sql_code_1)
             connect.commit()
-            #print('finish')
             s_20 = '''update "prd_db_dq"."01_dq_create_path" set status='''
             s_21 = "'"+str('DONE')+"' where row_id="+ str(rowi[2])
             sql_code_2 = s_20+s_21
--- Ubuntu/_01_insert_04_razdel_1st.py
+++ Ubuntu/_01_insert_04_razdel_1st.py
@@ -1,44 +1,45 @@
+#-*- coding: utf-8 -*-
+#!/usr/bin python3.5
+
 import psycopg2
 import csv
-#import codecs
+import codecs
 
-connect = psycopg2.connect(database='PJ', user='postgres', host='localhost', password='postgres')
+connect = psycopg2.connect(database='pj', user='admin_etl', host='localhost', password='Bc6f5201_etl')
 
 cursor = connect.cursor()
 
-cursor.execute('''SELECT count(1) FROM "PRD_DB_DQ"."01_dq_create_path"
+cursor.execute('''SELECT count(1) FROM "prd_db_dq"."01_dq_create_path"
                where prc_id = 6 and status='NONE' ''')
 
 k=0
 for row in cursor.fetchall():
     if row[0] > 0:
-        cursor.execute('''SELECT full_path, file_name,row_id FROM "PRD_DB_DQ"."01_dq_create_path"
+        cursor.execute('''SELECT full_path, file_name,row_id FROM "prd_db_dq"."01_dq_create_path"
                where prc_id = 6 and status='NONE' ''')
         for rowi in cursor.fetchall():
-            load_file = str(rowi[0]+"\\"+rowi[1])
-            load_file = load_file.replace(' C:\\','C:\\')
-            s_00 = '''update "PRD_DB_DQ"."01_dq_create_path" set status ='''
+            load_file = str(rowi[0]+"/"+rowi[1])
+            load_file = load_file.replace(' /','/')
+            s_00 = '''update "prd_db_dq"."01_dq_create_path" set status ='''
             s_01 = "'"+str('PROCESSING') + "' where row_id="+ str(rowi[2])
             sql_code_0 = s_00 + s_01
             cursor.execute(sql_code_0)
             connect.commit()
-            with open(load_file, 'r',encoding="utf8") as f:
+            with codecs.open(load_file, 'r',encoding='utf-8',errors='ignore') as f:
                 csv_data = csv.reader(f,delimiter='|') # читаем данные из файла и задаем разделитель ';'
-                #next(csv_data) # игнорируем 1 строку, содержащую описание столбцов в файле
-                #print(csv_data)
                 k=k+1
                 for row2 in csv_data:
                     #print(row2[1])
                     #print((row2))
                     #str(row2[0])
-                    s = "VALUES('"+str('test')+"',"+str(row2[0])+","+str(row2[1])+",'"+str(rowi[1])+"')"
-                    s0 =  'INSERT INTO "PRD_DB_STG"."04_razdel_city_1st" (city ,id ,cnt, file_name) '
+                    s = "VALUES("+str(row2[0])+","+str(row2[1])+","+str(row2[2])+",'"+str(rowi[1])+"')"
+                    s0 =  'INSERT INTO "prd_db_stg"."04_razdel_city_1st" (city ,id ,cnt, file_name) '
                     sql_code_1 = s0 + s         
                     #print(sql_code_1)
                     cursor.execute(sql_code_1)
             connect.commit()
             #print('finish')
-            s_20 = '''update "PRD_DB_DQ"."01_dq_create_path" set status='''
+            s_20 = '''update "prd_db_dq"."01_dq_create_path" set status='''
             s_21 = "'"+str('DONE')+"' where row_id="+ str(rowi[2])
             sql_code_2 = s_20+s_21
             cursor.execute(sql_code_2)
