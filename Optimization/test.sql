a8aafcd2f5d80bd2785c886209f5edec3a2af95b
Developer <klevtsovstanislav@gmial.com>
2016-12-05 21:29:45 +0300

    Create test.sql

---
+++ Optimization/test.sql
@@ -0,0 +1,66 @@
+ANALYZE
+
+WARNING:  skipping "test" --- only table or database owner can analyze it
+WARNING:  skipping "pg_statistic" --- only superuser or database owner can analyze it
+WARNING:  skipping "pg_type" --- only superuser or database owner can analyze it
+WARNING:  skipping "pg_authid" --- only superuser can analyze it
+WARNING:  skipping "pg_user_mapping" --- only superuser or database owner can analyze it
+WARNING:  skipping "pg_largeobject" --- only superuser or database owner can analyze it
+WARNING:  skipping "pg_attribute" --- only superuser or database owner can analyze it
+WARNING:  skipping "pg_proc" --- only superuser or database owner can analyze it
+WARNING:  skipping "pg_class" --- only superuser or database owner can analyze it
+WARNING:  skipping "pg_attrdef" --- only superuser or database owner can analyze it
+WARNING:  skipping "pg_constraint" --- only superuser or database owner can analyze it
+WARNING:  skipping "pg_inherits" --- only superuser or database owner can analyze it
+WARNING:  skipping "pg_index" --- only superuser or database owner can analyze it
+WARNING:  skipping "pg_operator" --- only superuser or database owner can analyze it
+WARNING:  skipping "pg_opfamily" --- only superuser or database owner can analyze it
+WARNING:  skipping "pg_opclass" --- only superuser or database owner can analyze it
+WARNING:  skipping "pg_am" --- only superuser or database owner can analyze it
+WARNING:  skipping "pg_amop" --- only superuser or database owner can analyze it
+WARNING:  skipping "pg_amproc" --- only superuser or database owner can analyze it
+WARNING:  skipping "pg_language" --- only superuser or database owner can analyze it
+WARNING:  skipping "pg_largeobject_metadata" --- only superuser or database owner can analyze it
+WARNING:  skipping "pg_aggregate" --- only superuser or database owner can analyze it
+WARNING:  skipping "pg_rewrite" --- only superuser or database owner can analyze it
+WARNING:  skipping "pg_trigger" --- only superuser or database owner can analyze it
+WARNING:  skipping "pg_event_trigger" --- only superuser or database owner can analyze it
+WARNING:  skipping "pg_description" --- only superuser or database owner can analyze it
+WARNING:  skipping "pg_cast" --- only superuser or database owner can analyze it
+WARNING:  skipping "pg_enum" --- only superuser or database owner can analyze it
+WARNING:  skipping "pg_namespace" --- only superuser or database owner can analyze it
+WARNING:  skipping "pg_conversion" --- only superuser or database owner can analyze it
+WARNING:  skipping "pg_depend" --- only superuser or database owner can analyze it
+WARNING:  skipping "pg_db_role_setting" --- only superuser can analyze it
+WARNING:  skipping "pg_tablespace" --- only superuser can analyze it
+WARNING:  skipping "pg_pltemplate" --- only superuser can analyze it
+WARNING:  skipping "pg_auth_members" --- only superuser can analyze it
+WARNING:  skipping "pg_shdepend" --- only superuser can analyze it
+WARNING:  skipping "pg_shdescription" --- only superuser can analyze it
+WARNING:  skipping "pg_ts_config" --- only superuser or database owner can analyze it
+WARNING:  skipping "pg_ts_config_map" --- only superuser or database owner can analyze it
+WARNING:  skipping "pg_ts_dict" --- only superuser or database owner can analyze it
+WARNING:  skipping "pg_ts_parser" --- only superuser or database owner can analyze it
+WARNING:  skipping "pg_ts_template" --- only superuser or database owner can analyze it
+WARNING:  skipping "pg_extension" --- only superuser or database owner can analyze it
+WARNING:  skipping "pg_foreign_data_wrapper" --- only superuser or database owner can analyze it
+WARNING:  skipping "pg_foreign_server" --- only superuser or database owner can analyze it
+WARNING:  skipping "pg_foreign_table" --- only superuser or database owner can analyze it
+WARNING:  skipping "pg_policy" --- only superuser or database owner can analyze it
+WARNING:  skipping "pg_replication_origin" --- only superuser can analyze it
+WARNING:  skipping "pg_default_acl" --- only superuser or database owner can analyze it
+WARNING:  skipping "pg_seclabel" --- only superuser or database owner can analyze it
+WARNING:  skipping "pg_shseclabel" --- only superuser can analyze it
+WARNING:  skipping "pg_collation" --- only superuser or database owner can analyze it
+WARNING:  skipping "pg_range" --- only superuser or database owner can analyze it
+WARNING:  skipping "pg_transform" --- only superuser or database owner can analyze it
+WARNING:  skipping "pg_database" --- only superuser can analyze it
+WARNING:  skipping "sql_features" --- only table or database owner can analyze it
+WARNING:  skipping "sql_implementation_info" --- only table or database owner can analyze it
+WARNING:  skipping "sql_languages" --- only table or database owner can analyze it
+WARNING:  skipping "sql_packages" --- only table or database owner can analyze it
+WARNING:  skipping "sql_parts" --- only table or database owner can analyze it
+WARNING:  skipping "sql_sizing" --- only table or database owner can analyze it
+WARNING:  skipping "sql_sizing_profiles" --- only table or database owner can analyze it
+Query returned successfully with no result in 03:42 minutes.
+
