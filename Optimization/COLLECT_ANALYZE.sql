f7862935476363d99fe4e057860ef974a37b1f03
Developer <klevtsovstanislav@gmial.com>
2016-12-05 23:20:11 +0300

    Create COLLECT_ANALYZE.sql

---
+++ Optimization/COLLECT_ANALYZE.sql
@@ -0,0 +1,41 @@
+ANALYZE prd_db_stg."01_objects_raw_data_1st";
+
+ANALYZE prd_db_stg."01_objects_raw_data_2nd";
+
+ANALYZE prd_db_stg."03_objects_with_squares_1st";
+
+ANALYZE prd_db_stg."03_objects_with_squares_2nd";
+
+ANALYZE prd_db_stg."04_podrazdel_city_1st";
+
+ANALYZE prd_db_stg."04_razdel_city_1st";
+
+ANALYZE prd_db_stg."04_rezdel_city_2nd";
+
+ANALYZE prd_db_stg."04_rubrica_city_1st";
+
+ANALYZE prd_db_stg."04_rubrica_city_2nd";
+
+ANALYZE prd_db_stg."04_tip_city_2nd";
+
+ANALYZE prd_db_stg."04_uni_org_city_1st";
+
+ANALYZE prd_db_stg."04_uni_org_city_2nd";
+
+--explain select count(*) from prd_db_stg."05_object_radiuses_rubrica_1st";
+
+--select count(*) from prd_db_stg."05_object_radiuses_rubrica_1st";
+
+ANALYZE prd_db_stg."05_object_radiuses_rubrica_1st";
+
+-- explain select count(*) from prd_db_stg."05_object_radiuses_rubrica_2nd";
+
+ANALYZE prd_db_stg."05_object_radiuses_rubrica_2nd";
+
+ANALYZE prd_db_stg."06_flat_city";
+
+ANALYZE prd_db_stg."07_stops_city";
+
+ANALYZE prd_db_stg."08_routers_city";
+
+ANALYZE prd_db_dq."01_dq_create_path";
