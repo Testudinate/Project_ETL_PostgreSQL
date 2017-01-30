ccbe03f542003b246fa3357adb861c7078acac68
Developer <klevtsovstanislav@gmial.com>
2017-01-30 23:38:15 +0300

    Create alter_table_add_day_id.sql

---
+++ PRD_DB_DM/TABLES/alter_table_add_day_id.sql
@@ -0,0 +1,26 @@
+
+alter table prd_db_dm."03_objects_with_squares_1st" add day_id date;
+
+alter table prd_db_dm."03_objects_with_squares_2nd" add day_id date;
+
+alter table prd_db_dm."04_podrazdel_city_1st" add day_id date;
+
+alter table prd_db_dm."04_razdel_city_1st" add day_id date;
+
+alter table prd_db_dm."04_rezdel_city_2nd" add day_id date; 
+
+alter table prd_db_dm."04_rubrica_city_1st" add day_id date;
+
+alter table prd_db_dm."04_rubrica_city_2nd" add day_id date;
+
+alter table prd_db_dm."04_tip_city_2nd" add day_id date;
+
+alter table prd_db_dm."04_uni_org_city_1st" add day_id date;
+
+alter table prd_db_dm."04_uni_org_city_2nd" add day_id date;
+
+alter table prd_db_dm."06_flat_city" add day_id date;
+
+alter table prd_db_dm."07_stops_city" add day_id date; 
+
+alter table prd_db_dm."08_routers_city" add day_id date;
