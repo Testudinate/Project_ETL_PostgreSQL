b2b11c3c3a410d377733db86cc4ab9e07c72b94a
Developer <klevtsovstanislav@gmial.com>
2017-01-30 23:05:12 +0300

    Create 04_load_rubrica_city_2nd.sql

---
+++ PRD_DB_DM/04_load_rubrica_city_2nd.sql
@@ -0,0 +1,54 @@
+--select prd_db_dm."03_load_objects_with_squares_1st"(21);
+
+-- select count(*) from prd_db_dm."04_rubrica_city_2nd"
+
+select prd_db_dm."04_load_rubrica_city_2nd"(21);
+
+-- Function: prd_db_dm."03_load_objects_with_squares_1st"(integer)
+
+-- DROP FUNCTION prd_db_dm."03_load_objects_with_squares_1st"(integer);
+
+CREATE OR REPLACE FUNCTION prd_db_dm."04_load_rubrica_city_2nd"(in_city integer)
+RETURNS integer AS $$
+DECLARE v_flag integer;
+        v_city integer;
+BEGIN
+
+select in_city into v_city;
+
+if v_city>=1 then 
+
+insert into prd_db_dm."04_rubrica_city_2nd" (
+  city ,
+  id ,
+  cnt ,
+  file_name 
+) 
+select 
+  city ,
+  id ,
+  cnt ,
+  file_name 
+from prd_db_stg."04_rubrica_city_2nd"
+where city = v_city;
+
+v_flag := 1;
+return v_flag;
+else 
+v_flag := -1;
+return v_flag;
+
+end if;
+commit;
+
+exception when others then
+begin
+v_flag:=0;
+return v_flag;
+end;
+
+END;
+$$ LANGUAGE plpgsql VOLATILE
+  COST 100;
+ALTER FUNCTION prd_db_dm."04_load_rubrica_city_2nd"(integer)
+OWNER TO admin_etl;
